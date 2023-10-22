79a6b2dfafbd4693ceca84a2eddc77fd91854474
msbeigiai <msbeigi83@gmail.com>
2023-10-22 17:38:40 +0300

    Create useBooks hook

---
+++ library-frontend/src/hooks/useBooks.ts
@@ -0,0 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Book {
+  id: number;
+  title: string;
+  author: string;
+  description: string;
+  copies: number;
+  copiesAvailable: number;
+  category: string;
+  image: string;
+}
+
+const useBooks = () => {
+  const fetchBooks = () =>
+    axios
+      .get<Book[]>("http://localhost:8080/api/v1/books")
+      .then((res) => res.data);
+
+  return useQuery<Book[], Error>({
+    queryKey: ["books"],
+    queryFn: fetchBooks,
+    staleTime: 10 * 1000,
+  });
+};
+
+export default useBooks;
