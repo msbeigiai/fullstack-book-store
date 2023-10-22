d3721f72cf66cf7c8c741fbd8733238036887657
msbeigiai <msbeigi83@gmail.com>
2023-10-22 17:27:50 +0300

    Create front end

---
+++ library-backend/src/main/java/com/msbeigi/librarybackend/config/DataRestConfig.java
@@ -0,0 +1,34 @@
+package com.msbeigi.librarybackend.config;
+
+import com.msbeigi.librarybackend.entity.Book;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] unsupportedActions = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.PATCH, HttpMethod.DELETE};
+        config.exposeIdsFor(Book.class);
+
+        disableHttpMethods(config, unsupportedActions);
+
+        String allowedOrigins = "http://localhost:5173";
+        cors.addMapping(config.getBasePath() + "/**")
+                .allowedOrigins(allowedOrigins);
+    }
+
+    private void disableHttpMethods(RepositoryRestConfiguration config,
+                                    HttpMethod[] unsupportedActions) {
+        config
+                .getExposureConfiguration()
+                .forDomainType(Book.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+    }
+}
