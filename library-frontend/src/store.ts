2fe4a8cd5e4ce7c07b86c7fe8dbbafb564ec6722
msbeigiai <msbeigi83@gmail.com>
2023-11-02 21:02:38 +0200

    Solve  category problem

--- library-frontend/src/store.ts
+++ library-frontend/src/store.ts
@@ -1,23 +1,22 @@
 import { create } from "zustand";
 
 interface BookQuery {
-    categoryId?: number;
-    searchText?: string;
+  categoryId?: number | null;
+  searchText?: string | null;
 }
 
 interface BookQueryStore {
-    bookQuery: BookQuery;
-    setCategoryId: (categoryId: number) => void;
-    setSearchText: (searchText: string) => void;
+  bookQuery: BookQuery;
+  setCategoryId: (categoryId: number | null) => void;
+  setSearchText: (searchText: string | null) => void;
 }
 
 const useBookQueryStore = create<BookQueryStore>((set) => ({
-    bookQuery: {},
-    setCategoryId: (categoryId) => set((store) =>
-        ({ bookQuery: { ...store.bookQuery, categoryId } })),
-    setSearchText: (searchText) => set((store) =>
-        ({ bookQuery: { ...store.bookQuery, searchText } })
-    )
+  bookQuery: {},
+  setCategoryId: (categoryId) =>
+    set((store) => ({ bookQuery: { ...store.bookQuery, categoryId } })),
+  setSearchText: (searchText) =>
+    set((store) => ({ bookQuery: { ...store.bookQuery, searchText } })),
 }));
 
 export default useBookQueryStore;
