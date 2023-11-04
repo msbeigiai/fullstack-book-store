426fae5fe08ba8d66af79b0beb021664d6d89053
msbeigiai <msbeigi83@gmail.com>
2023-11-04 15:47:15 +0200

    Add spring security to project

---
+++ library-backend/src/main/java/com/msbeigi/librarybackend/config/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.msbeigi.librarybackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private static final String GLOBAL_URI = "/api/v1";
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests((requests) -> requests
+                        .antMatchers(HttpMethod.GET, GLOBAL_URI + "/books/**")
+                        .permitAll()
+                        .antMatchers(HttpMethod.GET, GLOBAL_URI + "/categories/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated());
+        return http.build();
+
+    }
+}
