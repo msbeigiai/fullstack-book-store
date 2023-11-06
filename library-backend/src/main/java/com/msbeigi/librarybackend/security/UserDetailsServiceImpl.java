2f68377846ee9ac68c114af0acaa2aa04ef55695
msbeigiai <msbeigi83@gmail.com>
2023-11-06 22:04:58 +0200

    Implement UserDetailsServiceImpl

--- library-backend/src/main/java/com/msbeigi/librarybackend/security/UserDetailsServiceImpl.java
+++ library-backend/src/main/java/com/msbeigi/librarybackend/security/UserDetailsServiceImpl.java
@@ -1,12 +1,22 @@
 package com.msbeigi.librarybackend.security;
 
+import com.msbeigi.librarybackend.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return userRepository.findByEmailIgnoreCase(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User with email %s not found!".formatted(username)));
     }
 }
