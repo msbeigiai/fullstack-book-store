75bfb5a8e771bdcc2e45b627260a67a38cba2c99
msbeigiai <msbeigi83@gmail.com>
2023-11-12 20:28:33 +0200

    Add CsrfCookieFilter in SecurityFilterChain

--- library-backend/src/main/java/com/msbeigi/librarybackend/config/CsrfCookieFilter.java
+++ library-backend/src/main/java/com/msbeigi/librarybackend/config/CsrfCookieFilter.java
@@ -5,10 +5,12 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class CsrfCookieFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request,
--- library-backend/src/main/java/com/msbeigi/librarybackend/config/WebSecurityFilterChain.java
+++ library-backend/src/main/java/com/msbeigi/librarybackend/config/WebSecurityFilterChain.java
@@ -4,10 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,13 +20,27 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class WebSecurityFilterChain {
 
+    private final CsrfCookieFilter csrfCookieFilter;
+
     private static final List<String> PERMITTED_URI = Arrays.asList("/api/v1/categories", "/api/v1/register");
+
+    public WebSecurityFilterChain(CsrfCookieFilter csrfCookieFilter) {
+        this.csrfCookieFilter = csrfCookieFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler =
+                new CsrfTokenRequestAttributeHandler();
+        csrfTokenRequestAttributeHandler.setCsrfRequestAttributeName("_csrf");
+
         http
-                .csrf(AbstractHttpConfigurer::disable)
                 .cors(withDefaults())
+                .csrf(csrf -> csrf.csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+                        .ignoringRequestMatchers("/login")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
+                .addFilterAfter(csrfCookieFilter, BasicAuthenticationFilter.class)
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("api/v1/books")
                         .authenticated()
